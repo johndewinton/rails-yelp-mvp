a1b3ed2deee68716c362db7adbcbbaa544df2ecb
john de winton <john.dewinton@yahoo.co.uk>
2019-05-13 16:57:07 +0100

    completed raking

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,25 @@
 Rails.application.routes.draw do
+ resources :restaurants do
+  resources :reviews
+
+ end
+
+ # get "restaurants", to: "restaurants#index"
+
+ # get "restaurants/new", to: "restaurants#new"
+
+ # post "restaurants", to: "restaurants#create"
+
+ # get "restaurants/:id", to: "restaurants#show"
+
+ # get "restaurants/:id/edit", to: "restaurants#edit"
+ # patch "restaurants/:id", to: "restaurant#update"
+
+ # delete "restaurants/:id", to: "restaurants#destroy"
+
+ # get "reviews/new", to: "reviews#new"
+
+ # post "reviews", to: "reviews#create"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,45 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+# movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number:  '07888444555',
+    category:        'chinese'
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    phone_number:  'Pizzeria with industrial looks, serving rustic pizza and antipasti.',
+    category:        'japanese'
+  },
+    {
+    name:         'Buns & Bun',
+    address:      'Green Park',
+    phone_number:  'Pizzeria with industrial looks, serving rustic pizza and antipasti.',
+    category:        'italian'
+  },
+    {
+    name:         'KFC',
+    address:      'Picadilly Circus',
+    phone_number:  'Pizzeria with industrial looks, serving rustic pizza and antipasti.',
+    category:        'belgian'
+  },
+    {
+    name:         'Robuchon',
+    address:      'Hyde Park',
+    phone_number:  'Pizzeria with industrial looks, serving rustic pizza and antipasti.',
+    category:        'french'
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
